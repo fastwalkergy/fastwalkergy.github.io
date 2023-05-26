1a706fdc69cf114513c29f5055652d89aaf296a9
fastwalkergy <106460370+fastwalkergy@users.noreply.github.com>
2023-05-26 18:32:31 +0800

    c

---
+++ js/lk.js
@@ -0,0 +1,14 @@
+// 将旧域名和新域名存入变量中
+var oldDomain = "旧域名";
+var newDomain = "新域名";
+
+// 获取当前页面的 URL
+var currentUrl = window.location.href;
+
+// 如果当前 URL 包含旧域名，则替换为新域名
+if (currentUrl.indexOf(oldDomain) !== -1) {
+  var newUrl = currentUrl.replace(oldDomain, newDomain);
+  
+  // 自动重定向到新的 URL
+  window.location.replace(newUrl);
+}
